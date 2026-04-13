7395ae0e5af3e9636f48015fa4a8d0962a89fc1c
riflebird <riflebird@noreply.gitcode.com>
2026-04-13 20:41:16 +0800

    Add Short/UShort support for CoopMatHW constant expression
    Co-authored-by: GLM <noreply@z.ai>

--- spirv_glsl.cpp
+++ spirv_glsl.cpp
@@ -6046,6 +6046,14 @@ string CompilerGLSL::constant_expression(const SPIRConstant &c,
 			case SPIRType::UInt:
 				res += convert_to_string(c.scalar(0, i));
 				break;
+			case SPIRType::Short:
+				res += convert_to_string(c.scalar_i16(0, i));
+				res += backend.int16_t_literal_suffix;
+				break;
+			case SPIRType::UShort:
+				res += convert_to_string(c.scalar_u16(0, i));
+				res += backend.uint16_t_literal_suffix;
+				break;
 			default:
 				SPIRV_CROSS_THROW("Unsupported component type for CoopMatHW constant.");
 			}
@@ -15431,29 +15439,29 @@ void CompilerGLSL::emit_instruction(const Instruction &instruction)
 		break;
 	}
 
-		case OpCooperativeMatrixStoreHW:
-		{
-			if (length < 5)
-				SPIRV_CROSS_THROW("Not enough operands for OpCooperativeMatrixStoreHW.");
+	case OpCooperativeMatrixStoreHW:
+	{
+		if (length < 5)
+			SPIRV_CROSS_THROW("Not enough operands for OpCooperativeMatrixStoreHW.");
 
-			uint32_t ptr = ops[0];
-			uint32_t object = ops[1];
-			uint32_t src_shape = ops[2];
-			uint32_t src_offset = ops[3];
-			uint32_t layout_id = ops[4];
+		uint32_t ptr = ops[0];
+		uint32_t object = ops[1];
+		uint32_t src_shape = ops[2];
+		uint32_t src_offset = ops[3];
+		uint32_t layout_id = ops[4];
 
-			auto expr = to_expression(ptr);
-			auto &layout_const = get<SPIRConstant>(layout_id);
-			bool is_column_major = layout_const.scalar() != 0;
+		auto expr = to_expression(ptr);
+		auto &layout_const = get<SPIRConstant>(layout_id);
+		bool is_column_major = layout_const.scalar() != 0;
 
-			statement("coopMatStoreHW(", to_expression(object), ", ", expr, ", ",
-			          to_expression(src_shape), ", ",
-			          to_expression(src_offset), ", ",
-			          is_column_major ? "gl_CooperativeMatrixLayoutColumnMajorHW" : "gl_CooperativeMatrixLayoutRowMajorHW", ");");
+		statement("coopMatStoreHW(", to_expression(object), ", ", expr, ", ", to_expression(src_shape), ", ",
+		          to_expression(src_offset), ", ",
+		          is_column_major ? "gl_CooperativeMatrixLayoutColumnMajorHW" : "gl_CooperativeMatrixLayoutRowMajorHW",
+		          ");");
 
-			register_write(object);
-			break;
-		}
+		register_write(object);
+		break;
+	}
 
 	default:
 		statement("// unimplemented op ", instruction.op);
